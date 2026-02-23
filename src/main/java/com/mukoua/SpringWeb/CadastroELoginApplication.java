c757ca7c489ba89ecc7be33854816007168bd343
Mukoua <mukoua@gmail.com>
2026-02-22 22:22:02 -0300

    "feat: criando uma Dashboard com SpringWeb"

---
+++ src/main/java/com/mukoua/SpringWeb/CadastroELoginApplication.java
@@ -0,0 +1,13 @@
+package com.mukoua.SpringWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  CadastroELoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroELoginApplication.class, args);
+	}
+
+}
